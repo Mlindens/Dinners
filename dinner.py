a9dc0ca913828e53ec347bb98a3dbcc58294c0ba
Mlindens <maglinden@gmail.com>
2023-05-02 23:48:33 -0400

    -- Added exit functionality -- Cleaned up code and comments

--- dinner.py
+++ dinner.py
@@ -13,12 +13,13 @@ class Dinner:
 
     @staticmethod
     def display_menu():
-        print("Welcome to Whats for dinner!")
-        print("Choose your protein")
+        print("Choose your protein between 1-4.")
         print("1. Chicken")
         print("2. Beef")
         print("3. Fish")
         print("4. Pork")
+        print("Press S to save")
+        print("Press X to exit")
 
     @staticmethod
     def read_file(filename):
@@ -30,9 +31,15 @@ class Dinner:
 
     def choose_protein(self):
         choice = input("Choose your protein: ")
+        # return exit to quit program if user enters x
+        if choice.lower() == "x":
+            return "exit"
+        # get the filename associated with the user's choice
         filename = self.protein.get(choice)
+        # if a valid choice was made, read the file and return a random recipe name
         if filename:
             return self.read_file(filename)
         else:
+            # if the choice is invalid, display an error message and return None
             print("Pick a choice between 1-5.")
             return None
--- main.py
+++ main.py
@@ -2,14 +2,22 @@ from dinner import Dinner
 
 dinner = Dinner()
 
+print("Welcome to Whats for dinner!")
 
+
+# main loop
 def main():
-    dinner.display_menu()
-    read_file = None
-    while not read_file:
-        read_file = dinner.choose_protein()
-
-    print(read_file)
+    while True:
+        read_file = None
+        while not read_file:
+            dinner.display_menu()
+            read_file = dinner.choose_protein()
+            # TODO: add save option
+            # exit the loop and program if user enters exit
+            if read_file == "exit":
+                return
+        # show the randomly selected dish to user
+        print(f"\nDish selected: {read_file}\n")
 
 
 main()
