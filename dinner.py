c568e7eca26866c04834a2f0c074071dd8389d7b
Mlindens <maglinden@gmail.com>
2023-05-04 18:45:34 -0400

    -- Added instruction/ingredient options -- Cleaned up code and comments

--- dinner.py
+++ dinner.py
@@ -3,7 +3,25 @@ import json
 
 
 class Dinner:
+    """
+        Represents a dinner selection system based on protein type.
+
+        The class reads recipe data from JSON files, provides a menu for users to choose a protein,
+        and randomly selects a dish based on the user's choice. It also allows users to view the recipe and
+        ingredients of the selected dish.
+
+        Attributes:
+            protein (dict): A dictionary mapping user input to corresponding JSON files containing recipe data.
+
+        Methods:
+            display_menu: Displays the menu options for the user to choose a protein.
+            read_file: Reads the recipe data from a JSON file and returns a randomly selected dish name and its data.
+            show_recipe: Displays the recipe of the selected dish in a readable format.
+            show_ingredients: Displays the ingredients of the selected dish in a readable format.
+            choose_protein: Prompts the user to choose a protein and returns the corresponding dish name and data.
+        """
     def __init__(self):
+        # Initialize the protein attribute with a dictionary mapping user input to corresponding JSON files
         self.protein = {
             "1": "chicken.json",
             "2": "beef.json",
@@ -11,35 +29,50 @@ class Dinner:
             "4": "pork.json",
         }
 
-    @staticmethod
-    def display_menu():
+    def display_menu(self):
+        """Displays the menu options for the user to choose a protein."""
         print("Choose your protein between 1-4.")
         print("1. Chicken")
         print("2. Beef")
         print("3. Fish")
         print("4. Pork")
-        print("Press S to save")
-        print("Press X to exit")
+        print("Enter X to exit")
 
-    @staticmethod
-    def read_file(filename):
+    def read_file(self, filename):
+        """Reads the recipe data from a JSON file and returns a randomly selected dish name and its data."""
         with open(filename) as f:
             recipe_book = json.load(f)
             recipes = recipe_book["recipeBook"]["recipes"]
             random_recipe = random.choice(recipes)
-            return random_recipe["name"]
+            # Return a randomly selected dish name and its data
+            return random_recipe["name"], random_recipe
+
+    def show_recipe(self, dish_data):
+        """Displays the recipe of the selected dish in a readable format."""
+        print("\nRecipe:")
+        for index, step in enumerate(dish_data["instructions"], 1):
+            print(f"{index}. {step}")
+        print()
+
+    def show_ingredients(self, dish_data):
+        """Displays the ingredients of the selected dish in a readable format."""
+        print("\nIngredients:")
+        for ingredient in dish_data["ingredients"]:
+            print(f"{ingredient['name']}: {ingredient['quantity']}")
+        print()
 
     def choose_protein(self):
+        """Prompts the user to choose a protein and returns the corresponding dish name and data."""
         choice = input("Choose your protein: ")
-        # return exit to quit program if user enters x
+        # Return exit to quit program if user enters x
         if choice.lower() == "x":
             return "exit"
-        # get the filename associated with the user's choice
+        # Get the filename associated with the user's choice
         filename = self.protein.get(choice)
-        # if a valid choice was made, read the file and return a random recipe name
+        # If a valid choice was made, read the file and return a random recipe name
         if filename:
             return self.read_file(filename)
         else:
-            # if the choice is invalid, display an error message and return None
+            # If the choice is invalid, display an error message and return None
             print("Pick a choice between 1-5.")
             return None
--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 from dinner import Dinner
 
+"""
+This program helps users decide what to have for dinner by offering a selection of dishes based on protein type. 
+Users can choose between chicken, beef, fish, and pork dishes. The program then randomly selects a dish and 
+displays its name. Afterward, users have the option to view the recipe, view the ingredients, 
+or return to the main menu and choose a different protein.
+
+Usage:
+1. Run the program.
+2. Choose a protein option (1 - Chicken, 2 - Beef, 3 - Fish, 4 - Pork) or press 'X' to exit.
+3. The program will display a randomly selected dish based on the chosen protein.
+4. Enter 'R' to view the recipe, 'I' to view the ingredients, or 'G' to return to the main menu.
+5. Repeat steps 2-4 as desired or press 'X' to exit the program.
+"""
+
 dinner = Dinner()
 
 print("Welcome to Whats for dinner!")
@@ -12,12 +26,22 @@ def main():
         while not read_file:
             dinner.display_menu()
             read_file = dinner.choose_protein()
-            # TODO: add save option
-            # exit the loop and program if user enters exit
             if read_file == "exit":
                 return
-        # show the randomly selected dish to user
-        print(f"\nDish selected: {read_file}\n")
+        dish_name, dish_data = read_file  # Unpack dish name and data
+        print(f"\nDish selected: {dish_name}\n")
+
+        while True:
+            action = input("Enter 'R' to view recipe, 'I' to view ingredients, or 'G' to go back to menu: ").lower()
+            if action == 'r':
+                dinner.show_recipe(dish_data)
+            elif action == 'i':
+                dinner.show_ingredients(dish_data)
+            elif action == 'g':
+                break
+            else:
+                print("Invalid input. Please try again.")
 
 
 main()
+
